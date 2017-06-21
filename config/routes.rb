75f92fc322d9e37d678cdafee7ce3ddabe67a038
Yoshitaro Miura <xwarina.sivik@gmail.com>
2017-06-21 15:43:16 +0900

    Add assigned_notify

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resource :session, only: :create
-  
+  resource :assigned_notify, only: :create
+
   post "/ping" => ->(_env) { [200, { "Content-Type" => "text/plain" }, ["pong"]] }
 end
