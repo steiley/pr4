1e5bfdc079477c4d2e6300ea27e8c7758bb7d4d6
Yoshitaro Miura <xwarina.sivik@gmail.com>
2017-08-10 16:06:54 +0900

    Changed request path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resource :session, only: :create
-  resource :assigned_notify, only: :create
-  resource :random_assignment, only: :create
+  resource :random_request_review, only: :create
 
   post "/ping" => ->(_env) { [200, { "Content-Type" => "text/plain" }, ["pong"]] }
 end
