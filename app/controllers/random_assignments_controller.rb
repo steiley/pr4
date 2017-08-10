47dbb726f996df42af6e97e51ce1e688c5170b75
Yoshitaro Miura <xwarina.sivik@gmail.com>
2017-08-10 15:01:58 +0900

    Fixed items pointed out by rubocop

--- app/controllers/random_assignments_controller.rb
+++ app/controllers/random_assignments_controller.rb
@@ -6,25 +6,80 @@ class RandomAssignmentsController < ApplicationController
       return
     end
 
-    pr_user_login = payload["pull_request"]["user"]["login"]
-    http = GraphQL::Client::HTTP.new("https://api.github.com/graphql")do
+    repository_info = get_repository_info(http)
+    response = request_reviewer(conn, payload, repository_info)
+
+    render json: { status: :ok } if response.status == 201
+
+    raise "#{response.status}:#{response.body}"
+  end
+
+  private
+
+  def bear_graphql_http(payload)
+    GraphQL::Client::HTTP.new("https://api.github.com/graphql") do
       attr_writer :token
 
       def headers(_context)
         {
-         Authorization: "token #{@token}",
-         Accept: "application/vnd.github.machine-man-preview+json"
+          Authorization: "token #{@token}",
+          Accept: "application/vnd.github.machine-man-preview+json"
         }
       end
     end
     http.token = AuthenticateService.perform(payload["installation"]["id"])
+    http
+  end
+
+  def execute_graphql_http(http, document, variables = {})
+    response = http.execute(document: document, variables: variables)
+
+    raise response["errors"].first["message"] if response["errors"]
+
+    response["data"]
+  end
+
+  def bear_faraday_connection
+    Faraday.new(url: "https://api.github.com/") do |faraday|
+      faraday.basic_auth("pr3-bot", IO.read("pr3-bot.key").strip)
+      faraday.response :logger, Rails.logger
+      faraday.adapter :net_http
+    end
+  end
+
+  def pickup_reviewer(payload, repository_info)
+    pr_user_login = payload["pull_request"]["user"]["login"]
+
+    member_logins = repository_info["mentionableUsers"]["nodes"].map { |node| node["login"] }
+    member_logins.delete(pr_user_login)
+    member_logins.delete("pr3-bot")
+    member_logins.sample
+  end
+
+  def request_reviewer(conn, payload, repository_info)
+    owner_login = payload["repository"]["owner"]["login"]
+
+    conn = bear_faraday_connection
+    conn.post do |req|
+      req.url(
+        %(/repos/#{owner_login}/#{payload["repository"]["name"]}/pulls/#{pr_number(payload)}/requested_reviewers")
+      )
+
+      req.headers["Accept"] = "application/vnd.github.thor-preview+json"
+      req.body = JSON.generate(reviewers: [pickup_reviewer(payload, repository_info)])
+    end
+  end
+
+  def get_repository_info(http, payload)
+    http = bear_graphql_http(payload)
 
     repository_owner = payload["repository"]["owner"]["login"]
-    pr_number = payload["pull_request"]["number"]
-    repository_info = graphql_http_execute(http, <<-QUERY)["repository"]
+
+    execute_graphql_http(http, <<-QUERY)["repository"]
+
          query {
            repository(owner: "#{repository_owner}", name: "#{payload["repository"]["name"]}") {
-             pullRequest(number: #{pr_number}) {
+             pullRequest(number: #{pr_number(payload)}) {
                id
              }
              mentionableUsers(last: 10) {
@@ -35,44 +90,9 @@ class RandomAssignmentsController < ApplicationController
            }
          }
     QUERY
-
-    member_logins = repository_info["mentionableUsers"]["nodes"].map{ |node|node["login"] }
-    member_logins.delete(pr_user_login)
-    member_logins.delete("pr3-bot")
-
-    conn = Faraday.new(url: "https://api.github.com/") do |faraday|
-      faraday.basic_auth("pr3-bot", IO.read("pr3-bot.key").strip)
-      faraday.response :logger, Rails.logger
-      faraday.adapter :net_http
-    end
-
-    owner_login = payload["repository"]["owner"]["login"]
-    faraday_response = conn.post do |req|
-      req.url("/repos/#{owner_login}/#{payload["repository"]["name"]}/pulls/#{pr_number}/requested_reviewers")
-
-      req.headers["Accept"] = "application/vnd.github.thor-preview+json"
-      req.body = JSON.generate({
-        reviewers: [
-          member_logins.sample
-        ]
-      }
-      )
-    end
-
-    if faraday_response.status == 201
-      render json: {status: :ok}
-    else
-      raise %Q(#{response.status}:#{response.body})
-    end
   end
 
-  private
-  def graphql_http_execute(http, document, variables = {})
-    response = http.execute(document: document, variables: variables)
-    if response["errors"]
-      raise response["errors"].first["message"]
-    else
-      response["data"]
-    end
+  def pr_number(payload)
+    payload["pull_request"]["number"]
   end
 end
--- app/services/authenticate_service.rb
+++ app/services/authenticate_service.rb
@@ -2,24 +2,30 @@ module AuthenticateService
   extend self
 
   def perform(installation_id)
-    require 'net/http'
-    require 'uri'
+    request = access_tokens_request(installation_id)
 
+    response = access_tokens_response(request)
+
+    return JSON.parse(response.body)["token"] if response.code.to_i == 201
+
+    raise "#{response.code} #{response.body}"
+  end
+
+  private
+
+  def access_tokens_request(installation_id)
     uri = URI.parse("https://api.github.com/installations/#{installation_id}/access_tokens")
     request = Net::HTTP::Post.new(uri.path)
     request["Authorization"] = "Bearer #{GenerateGithubAppsJwtService.perform}"
     request["Accept"] = "application/vnd.github.machine-man-preview+json"
+    request
+  end
 
+  def access_tokens_response(request)
     http = Net::HTTP.new(uri.hostname, uri.port)
     http.use_ssl = true
-    http.set_debug_output($stdout)
-    response = http.start do |h|
+    http.start do |h|
       h.request(request)
-    end
-    if response.code.to_i == 201
-      return JSON.parse(response.body)["token"]
-    else
-      raise "#{response.code} #{response.body}"
     end
   end
 end
