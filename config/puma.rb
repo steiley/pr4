b9ef6e8e2ee6414ea43c5b31739411b659b2c2f6
Yoshitaro Miura <xwarina.sivik@gmail.com>
2017-08-15 17:54:28 +0900

    auto correct Style/StringLiterals

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 port        ENV.fetch("PORT") { 3500 }
 daemonize false
-state_path 'tmp/pids/puma.state'
-pidfile 'tmp/pids/puma.pid'
+state_path "tmp/pids/puma.state"
+pidfile "tmp/pids/puma.pid"
 threads 0,16
 workers 4
 environment ENV.fetch("RAILS_ENV") { "development" }
